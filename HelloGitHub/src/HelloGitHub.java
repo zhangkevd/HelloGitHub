dd980d14eab63eb62bd0fb9f91f180d83365ddb8
zhang-j1b3 <zhangkevd@163.com>
2013-03-19 12:37:19 +0800

    add a line ->"the src is update"

--- HelloGitHub/src/HelloGitHub.java
+++ HelloGitHub/src/HelloGitHub.java
@@ -7,5 +7,7 @@ public class HelloGitHub {
         System.out.println("Say HelloGitHub to the GitHub from zhang - j1b3");
         System.out.println(new Date());
         System.out.println("-----------------------------------------------");
+
+        System.out.println("the src is update !");
     }
 }
