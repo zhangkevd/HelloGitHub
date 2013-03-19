b8ca9fabf52caea1f0b18153ca8ff121cfc1d6fc
zhang-j1b3 <zhangkevd@163.com>
2013-03-19 11:42:20 +0800

    commit 2 for test by zhang - j1b3

---
+++ HelloGitHub/src/HelloGitHub.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class HelloGitHub {
+    public static void main(String[] args) {
+
+        System.out.println("-----------------------------------------------");
+        System.out.println("Say HelloGitHub to the GitHub from zhang - j1b3");
+        System.out.println(new Date());
+        System.out.println("-----------------------------------------------");
+    }
+}
